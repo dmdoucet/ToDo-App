75285aba865aa44a4d31f082bcb67f7b672d833e
Dylan Doucet <28883259+dmdoucet@users.noreply.github.com>
2023-12-13 19:35:43 -0800

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ function addTask() {
 listContainer.addEventListener('click', function(e){
     if(e.target.tagName === "LI") {
         e.target.classList.toggle('checked');
+        saveData();
     } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
+        saveData();
     }
 }, false);
 
